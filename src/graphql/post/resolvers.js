9c5ec8ff478bf0d267298fc574edc8d8ff948c3c
rmc3408 <rmc3408@protonmail.com>
2022-08-16 22:18:32 -0400

    section 3 done

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -4,21 +4,54 @@ export const postResolvers = {
   Query: {
     getPost: async (parent, args, context, info) => {
       const data = await context.fetchPosts('/' + args.id);
-      return data.json();
+      const convData = await data.json();
+      
+      // if (Math.random() > 0.5) {
+      //   return {
+      //     statusCode: 500,
+      //     message: 'Post took too long',
+      //     timeout: 50,
+      //   };
+      // }
+      
+      // if (!convData.id) {
+      //   return {
+      //     statusCode: 404,
+      //     message: 'Post not found',
+      //     postId: args.id
+      //   };
+      // }
+
+      return convData;
     },
     getPosts: async (parent, args, context, info) => {
       //console.log(args.input)
-      const filterArgsPosts = new URLSearchParams(args.input)
+      const filterArgsPosts = new URLSearchParams(args.input);
       //console.log(filterArgsPosts.toString())
       const data = await context.fetchPosts('/?' + filterArgsPosts);
       return data.json();
     },
   },
-  Post: {
-    unixTimestamp: (parent, args, context, info) => {
-      //console.log(parent.createdAt);
-      const newTime = new Date(parent.createdAt).getSeconds();
-      return newTime;
-    }
-  }
+  // Post: {
+  //   unixTimestamp: (parent, args, context, info) => {
+  //     //console.log(parent.createdAt);
+  //     const newTime = new Date(parent.createdAt).getSeconds();
+  //     return newTime;
+  //   },
+  // },
+  // PostResult: {
+  //   __resolveType: (obj) => {
+  //     if (typeof obj.postId !== 'undefined') return "PostNotfoundError";
+  //     if (typeof obj.timeout !== 'undefined') return "PostTimeOutError";
+  //     if (typeof obj.id !== 'undefined') return "Post";
+  //     return null;
+  //   },
+  // },
+  // PostError: {
+  //   __resolveType: (obj) => {
+  //     if (typeof obj.postId !== 'undefined') return "PostNotfoundError";
+  //     if (typeof obj.timeout !== 'undefined') return "PostTimeOutError";
+  //     return null;
+  //   },
+  // },
 };
--- src/graphql/post/type.js
+++ src/graphql/post/type.js
@@ -9,11 +9,30 @@ export const postTypeDefs = gql`
     indexRef: Int!
     createdAt: String!
     #user: User!
-    unixTimestamp: String!
+    #unixTimestamp: String!
   }
 
   extend type Query {
-    getPost(id: ID!): Post!
+    getPost(id: ID!): Post! #PostResult!
     getPosts(input: filterInput): [Post!]!
   }
+
+  # union PostResult = Post | PostNotfoundError | PostTimeOutError
+
+  # interface PostError {
+  #   statusCode: Int!
+  #   message: String!
+  # }
+
+  # type PostTimeOutError implements PostError {
+  #   statusCode: Int!
+  #   message: String!
+  #   timeout: Int!
+  # }
+
+  # type PostNotfoundError implements PostError {
+  #   statusCode: Int!
+  #   message: String!
+  #   postId: String!
+  # }
 `;
