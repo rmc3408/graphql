0c2d261e3e7e5885d2831abd180bfbb3d24fc1be
Raphael <rmc3408@protonmail.com>
2022-10-09 09:03:35 -0400

    formatted

--- knex/query/A002.js
+++ knex/query/A002.js
@@ -2,47 +2,56 @@ const knex = require('../db');
 
 
 // SELECT * FROM USERS;
-// knex('users')
-//   .then(data => console.log(data))
-//   .catch(e => console.log(e.message))
-//   .finally(()=> knex.destroy());
+knex('users')
+  .then(data => console.log(data))
+  .catch(e => console.log(e.message))
+  .finally(()=> knex.destroy());
 
 
 // SELECT email FROM USERS;
-// knex('users as u')
-//   .select('u.email as user_email')
-//     .then(data => console.log(data))
-//     .catch(e => console.log(e.message))
-//     .finally(()=> knex.destroy());
+knex('users as u')
+  .select('u.email as user_email')
+    .then(data => console.log(data))
+    .catch(e => console.log(e.message))
+    .finally(()=> knex.destroy());
 
 
 // // SELECT id, first_name from users WHERE first_name='Raphael';
-// knex('users as u')
-//   .select('id', 'first_name')
-//   .where('first_name', 'Raphael')
-//     .then(data => console.log(data))
-//     .catch(e => console.log(e.message))
-//     .finally(()=> knex.destroy());
+knex('users as u')
+  .select('id', 'first_name')
+  .where('first_name', 'Raphael')
+    .then(data => console.log(data))
+    .catch(e => console.log(e.message))
+    .finally(()=> knex.destroy());
 
 
 // SELECT * from users WHERE id BETWEEN 1 AND 4 OR id BETWEEN 80 AND 83;
-// knex('users as u')
-//   .select('*')
-//   .whereBetween('id', [1, 4])
-//   .orWhereBetween('id', [80, 83])
-//     .then(data => console.log(data))
-//     .catch(e => console.log(e.message))
-//     .finally(()=> knex.destroy());
+knex('users as u')
+  .select('*')
+  .whereBetween('id', [1, 4])
+  .orWhereBetween('id', [80, 83])
+    .then(data => console.log(data))
+    .catch(e => console.log(e.message))
+    .finally(()=> knex.destroy());
 
 // SELECT * from users WHERE id IN (20,30);
-// knex('users as u')
-//   .select('*')
-//   .whereIN('id', [20, 30])
-//     .then(data => console.log(data))
-//     .catch(e => console.log(e.message))
-//     .finally(()=> knex.destroy());
+knex('users as u')
+  .select('*')
+  .whereIN('id', [20, 30])
+    .then(data => console.log(data))
+    .catch(e => console.log(e.message))
+    .finally(()=> knex.destroy());
 
-// SELECT first_name from users WHERE first_name LIKE '%na';
+// SELECT id, first_name from users WHERE first_name LIKE '%na' ORDER BY id ASC;
+knex('users as u')
+.select('u.first_name as names')
+.whereILike('u.first_name', '%na')
+  .then(data => console.log(data))
+  .catch(e => console.log(e.message))
+  .finally(()=> knex.destroy());
+
+
+// SELECT id, first_name from users WHERE first_name LIKE '%a' ORDER BY id ASC LIMIT 10;
 knex('users as u')
 .select('u.first_name as names')
 .whereILike('u.first_name', '%na')
